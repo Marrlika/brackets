74516794d17ee9f8a8d1d3ee9090587dd94160eb
Marrlika <Marrlika@outlook.com>
2020-03-17 12:31:30 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let stackBracket = [];
+ let openBracket;
+ let singleBracket = 0;
+ str.split("").forEach(bracket =>{
+   if(singleBracket > 0) {
+     return;
+    }
+  bracketsConfig.forEach(element => {
+    if(element[1] == bracket) {
+      openBracket = stackBracket.pop();
+      if (openBracket!==element[0]) {
+        if (element[0] == bracket) {
+          if(openBracket) {
+            stackBracket.push(openBracket);}
+          stackBracket.push(bracket);
+        } else {
+          singleBracket++;
+        }
+
+      }
+    }else if(element[0] == bracket) {
+      stackBracket.push(bracket);
+  }
+  });
+ });
+ 
+ if (stackBracket.length>0||singleBracket > 0) {
+  return false;
+ } else {return true;}
+
+
 }
+
